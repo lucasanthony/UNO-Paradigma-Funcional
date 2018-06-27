38e1cee19e8c5bc5841dbbddbf0c288ac62d8df3
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-06-27 18:46:28 -0300

    ideia de função

--- bot.hs
+++ bot.hs
@@ -1,1 +1,6 @@
 -- a implementar...
+
+{- Função que recebe o deck do bot e o size do proximo jogador,
+verifica se o jogo está perigoso ou não, e então retorna um Int
+que representará a posição da carta a ser jogada do seu deck, a
+partir daí, a função gerenciaBot assume o comando -}
